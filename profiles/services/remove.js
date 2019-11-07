a33bf13142f1bbd0ed7d133c78cf586150104cba
sadbut_1 <eduardommoya@gmail.com>
2019-11-07 19:15:28 -0300

    first commit

--- profiles/services/remove.js
+++
@@ -1,7 +0,0 @@
-import Profile from '../models/Profile'
-
-export default (req, res) => {
-    Profile.findByIdAndRemove(req.params.id)
-        .then(() => res.status(204).end())
-        .catch(err => res.status(500).json({ status: false, data: {}}))
-}
